bbbd21e1a192f8b945196336315f482b136e9967
CoderRedwing <ajiteshru@gmail.com>
2025-03-06 18:02:13 +0530

    some changes

--- api/server.js
+++ api/server.js
@@ -1,6 +1,13 @@
 const app = require("./app");
+const cors = require("cors");
 
 const PORT = process.env.PORT || 4000;
+
+app.use(cors());
+
+app.get("/", (req, res) => { 
+    res.json("Hi all Good");
+});
 
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
